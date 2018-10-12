19d87e3d48e6ecc2e3255dfe25614526d7389795
Nicklas Botö <nicklas.boto@gmail.com>
2018-10-12 13:57:33 +0200

    Add files via upload

--- Tetris.hs
+++ Tetris.hs
@@ -32,13 +32,10 @@ data Tetris = Tetris (Vector,Shape) Shape [Shape]
 type Vector = (Int,Int)
 
 -- | The size of the well
-wellWidth, wellHeight :: Int
-wellWidth = 10
-wellHeight = 20
-
 wellSize :: (Int,Int)
 wellSize = (wellWidth,wellHeight)
-
+wellWidth = 10
+wellHeight = 20
 
 -- | Starting position for falling pieces
 startPosition :: Vector
@@ -53,82 +50,60 @@ place :: (Vector,Shape) -> Shape
 place (v,s) = shiftShape v s
 
 
--- prop_Tetris return a True value if prop_Shape returns
--- True and the size of the well is equal to the shapeSize of the well
-
+-- | An invariant that startTetris and stepTetris should uphold
 prop_Tetris :: Tetris -> Bool
 prop_Tetris (Tetris (p, s) w r) = prop_Shape s && shapeSize w == wellSize
 
 
--- addWalls adds squares of black around the
--- input shape by rotating it 4 times, adding
--- the wall pieces one side at a time
+-- | Add black walls around a shape
+createWall :: Int -> Row
+createWall l = replicate l (Just Black)
 
 addWall :: Shape -> Shape
-addWall (S (s:ss)) = S $ (s:ss) ++ [createWall]
-  where createWall = replicate (length s) (Just Black)
+addWall (S (s:ss)) = S $ (s:ss) ++ [createWall $ length s]
 
 addWalls :: Shape -> Shape
 addWalls s = iterate (rotateShape . addWall) s !! 4
 
-
--- drawTetris creates the game field by combining
--- the first falling shape and the well
-
+-- | Visualize the current game state. This is what the user will see
+-- when playing the game.
 drawTetris :: Tetris -> Shape
 drawTetris (Tetris (v,p) w _) = addWalls $ combine (shiftShape v p) w
 
+chooseShape :: [Double] -> [Int]
+chooseShape s = map (\x -> floor (6.99*x)) s
 
--- startTetris creates a simple initialization of the game,
--- putting a shapes from allShapes in the well, with the
--- index 0-6, depending on the input double.
-
+-- | The initial game state
 startTetris :: [Double] -> Tetris
 startTetris rs = Tetris (startPosition,shape1) (emptyShape wellSize) supply
   where
-    shape1:supply = map (allShapes!!) (map floor (map (*6.99) rs))
-    -- BÄTTRE SÄTT ATT FIXA RANDOM? enbart 0.5 för lägsta och högsta
-      --repeat (allShapes!!round(rs*(length(allShapes)-1)))
+    shape1:supply = zipWith (!!) (repeat allShapes) (chooseShape rs)
 
 
--- stepTetris reacts to user input and moves the
--- piece accordingly
-
+-- | React to input. The function returns 'Nothing' when it's game over,
+-- and @'Just' (n,t)@, when the game continues in a new state @t@.
 stepTetris :: Action -> Tetris -> Maybe (Int,Tetris)
-stepTetris MoveDown t  = tick t
-stepTetris MoveLeft t  = Just (0, movePiece (-1) t)
-stepTetris MoveRight t = Just (0, movePiece 1 t)
-stepTetris Rotate t    = Just (0, rotatePiece t)
-
-stepTetris _ t      = tick t
-
-
--- "move" adds the input Vector with the current state of the game
--- which will move the currently falling piece accordingly
+stepTetris MoveDown  t = tick t
+stepTetris MoveLeft  t = Just (0 , movePiece (-1) t)
+stepTetris MoveRight t = Just (0 , movePiece 1 t)
+stepTetris Rotate    t = Just (0, rotatePiece t)
+stepTetris Slam      t = Just (0, slamPiece 1 t)
+stepTetris _         t = tick t
 
 move :: Vector -> Tetris -> Tetris
-move v1 (Tetris (v2, p) w r) = Tetris (v1 `vAdd` v2, p) w r
-
-
--- "tick" makes sure that the game actually changes according
--- to the user input. Guards are put in to make sure
--- that the falling piece does not move further down
--- when it has reached the bottom.
+move v1 (Tetris (v2, p) w r) = Tetris (vAdd v1 v2, p) w r
 
 tick :: Tetris -> Maybe (Int, Tetris)
 tick t
   | collision update = dropNewPiece t
-  | otherwise        = Just (0, update)
+  | otherwise        = Just (0, move (0, 1) t)
     where update = move (0, 1) t
 
-{-
-
--- ** C01
-
--}
-
--- collision tests if the piece overlaps with any side
--- of the well or other placed shape
+slamPiece :: Int -> Tetris -> Tetris
+slamPiece s t
+  | collision $ slam s = move (0, s-1) t
+  | otherwise          = slamPiece (s+1) t
+    where slam x = move (0, x) t
 
 collision :: Tetris -> Bool
 collision (Tetris ((x, y), p) w r)
@@ -139,91 +114,39 @@ collision (Tetris ((x, y), p) w r)
   | otherwise                      = False
     where (width, height) = shapeSize p
 
-{-
-
--- ** C03
-
--}
-
--- movePiece is used for horizontal movement of
--- the falling piece. If the shape is colliding
--- with either the left or right side, it will not
--- move further to each respective way.
-
 movePiece :: Int -> Tetris -> Tetris
-movePiece n t
-  | collision newPos = t
-  | otherwise        = newPos
-  where newPos = move (n, 0) t
-
-{-
-
--- ** C04
-
-&
-
--- ** C06
-
--}
-
--- rotate is rotates the falling shape of the input Tetris,
--- regardless if it collides with anything.
+movePiece dir t
+  | collision nv = t
+  | otherwise    = nv
+    where nv = move (dir, 0) t
 
 rotate :: Tetris -> Tetris
-rotate (Tetris (v, p) w r) = Tetris (v, rotateShape(p)) w r
-
-
--- rotatePiece takes a Tetris and checks if any collision
--- occurs when the falling shape is rotated.
-
-rotatePiece :: Tetris -> Tetris
-rotatePiece t
-  | collision rotPos = adjust (rotate t)
-  | otherwise        = rotPos
-  where rotPos = rotate t
-
-{-
-
--- ** C05
-
--}
-
--- adjust checks if the rotated shape collides with any sides
--- or the bottom of the well, and places the rotated shape
--- accordingly, instead of blocking the rotation.
+rotate (Tetris (v, p) w r) = Tetris (v, rotateShape p) w r
 
 adjust :: Tetris -> Tetris
 adjust (Tetris ((x, y), p) w r)
   | (x + width) > wellWidth   = (Tetris ((wellWidth-width, y), p) w r)
   | (y + height) > wellHeight = (Tetris ((x, y-(height-1)), p) w r)
   | otherwise                 = (Tetris ((x, y), p) w r)
-  where (width,height) = shapeSize p
+    where (width, height)     = shapeSize p
 
+rotatePiece :: Tetris -> Tetris
+rotatePiece t
+  | collision rot = adjust rot
+  | otherwise     = rot
+    where rot = rotate t
 
-{-
-
--- ** C07
-
--}
-
--- dropNewPiece is called when the currently falling piece
--- collides with something downwards, meaning it shall be
--- placed there. A piece from the supply of shapes is used
--- from the startPosition, while the well is shanged to include
--- the previously falling shape.
-
-dropNewPiece :: Tetris -> Maybe (Int,Tetris)
+dropNewPiece :: Tetris -> Maybe (Int, Tetris)
 dropNewPiece (Tetris (v, p) w (r:rs))
-  | collision newPiece = Nothing
-  | otherwise          = Just (0, newPiece)
-  where newPiece = (Tetris (startPosition, r) (combine w (place(v,p))) rs)
-      --  (n,_)        =
+  | overlaps nw (place np) = Nothing
+  | otherwise              = Just (sc, (Tetris np nw rs))
+    where (sc, nw) = clearLines $ combine (place (v, p)) w
+          np       = (startPosition, r)
 
+isComplete :: Row -> Bool
+isComplete = all (/= Nothing)
 
-{-
-
--- ** C08
-
--}
-
--- clearLines :: Shape -> (Int, Shape)
+clearLines :: Shape -> (Int, Shape)
+clearLines (S r) = (clr, shiftShape (0, clr) $ S (filter (notComplete) r))
+  where clr = length $ filter (isComplete) r
+        notComplete x = not $ isComplete x
